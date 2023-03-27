616087b9993f1cadfe3aa75f3a7df7e660203394
mspolermo <mspolermo@gmail.com>
2023-03-27 15:24:05 +0500

    JSON was added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
+import goods from './goods/goods.json'
+
+interface goods {
+  id: number,
+  image: string,
+  title: string,
+  sizeType: string,
+  size: number,
+  barcode: number,
+  producer: string,
+  brand: string,
+  desription: string,
+  price: number
+}
+
+
 
 
 function App() {
   return (
     <div>
-      <p>Hello world. It's me! </p>
+      <h1>Unbelievable!</h1>
+      {goods.map( good => {
+        return (
+          <div>{good.producer}</div>
+        )
+      })}
     </div>
   );
 }
